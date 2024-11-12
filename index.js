966843cad4af96a54c04bdfec2da1098f85d7b20
NoobYoup <134846824+NoobYoup@users.noreply.github.com>
2024-11-12 22:51:14 +0700

    create cors

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require("express");
+const express = require('express');
+
+const cors = require('cors');
+
 const app = express();
 const port = 3300;
 
-const database = require("./config/database");
+const database = require('./config/database');
 database.connect();
 
-const Song = require("./model/song.model");
-app.get("/", async (req, res) => {
-  const listSong = await Song.find({});
+app.use(cors());
 
-  console.log(listSong);
+const Song = require('./model/song.model');
+app.get('/', async (req, res) => {
+    const listSong = await Song.find({});
 
-  res.json({
-    code: 200,
-    songs: listSong,
-  });
+    console.log(listSong);
+
+    res.json({
+        code: 200,
+        songs: listSong,
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
